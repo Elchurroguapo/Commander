aeeedf21effbfbff4d8785b03d0eda43fdb1ee2f
Elchurroguapo <95740334+Elchurroguapo@users.noreply.github.com>
2022-03-24 22:55:45 -0700

    Create KVM-start.sh

---
+++ KVM-start.sh
@@ -0,0 +1,37 @@
+nano /etc/libvirt/hooks/qemu.d/win10/prepare/begin/start.sh
+  
+  #debugging
+  set -x
+  
+  #load variables we defined
+  source "/etc/libvirt/hooks/kvm.conf"
+  
+  #stop display manager
+  systemctl stop sddm.service
+  
+  #Unbind VTConsoles
+  echo 0 > /sys/class/vtconsole/vtcon0/bind
+  echo 0 > /sys/class/vtconsole/vtcon1/bind
+  
+  #Unbind EFI-framebuffer
+  echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
+  
+  #Avoid race condition
+  sleep 10
+  
+  #Unload Nvidia
+  modprobe -r nvidia_drm
+  modprobe -r nvidia_modeset
+  modprobe -r drm_kms_helper
+  modprobe -r nvidia
+  modprobe -r i2c_nvidia_gpu
+  modprobe -r nvidia_uvm
+  
+  #Unbind gpu
+  virsh nodedev-detach $VIRSH_GPU_VIDEO
+  virsh nodedev-detach $VIRSH_GPU_AUDIO
+  
+  #Load VFIO
+  modprobe vfio
+  modprobe vfio_pci
+  modprobe vfio_iommu_type1
