1db94b8f9bbb83d18075fcbf5a62751fe140a9ab
Elchurroguapo <95740334+Elchurroguapo@users.noreply.github.com>
2022-04-30 23:47:52 -0400

    Delete xorg-install.sh

--- xorg-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo pacman -Syy
-sudo pacman -S xf86-video-fbdev xf86-video-vesa xf86-video-nouveau mesa lib32-mesa
-sudo pacman -Syu
-sudo pacman -S xorg-server xorg-apps xorg-xinit nitrogen firefox git
-sudo pacman -S git
-git clone https://aur.archlinux.org/yay.git
-cd yay/
-makepkg -csi
